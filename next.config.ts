4ce47ac1df592d2327bd779391bd2fd3f934739d
Kochkina Ekaterina <gudrum1983@gmail.com>
2025-06-23 13:51:18 +0200

    обновила конфиг, попытка деплоя

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/o_complex",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
